9b4c92ac5639e447c31fa679f0f3e7d4743afedd
Prabhat ranjan <72509632+prabhatranjan15@users.noreply.github.com>
2021-01-25 14:28:36 +0530

    Create Print Pretty.cpp

---
+++ Print Pretty.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+	int T; cin >> T;
+	cout << setiosflags(ios::uppercase);
+	cout << setw(0xf) << internal;
+	while(T--) {
+		double A; cin >> A;
+    	double B; cin >> B;
+		double C; cin >> C;
+
+		/* Enter your code here */
+    cout << std::hex << std::left << std::showbase << std::nouppercase; // formatting
+cout << (long long) A << endl; // actual printed part
+cout << std::dec << std::right << std::setw(15) << std::setfill('_') << std::showpos << std::fixed << std::setprecision(2); // formatting
+cout << B << endl; // actual printed part
+cout << std::scientific << std::uppercase << std::noshowpos << std::setprecision(9); // formatting
+cout << C << endl; // actual printed part
+
+	}
+	return 0;
+
+}
